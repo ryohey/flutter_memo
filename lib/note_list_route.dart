13ab806a18049137d48bc6b8b319023b72777d25
ryohey <info@codingcafe.jp>
2020-07-09 19:10:57 +0900

    Add AddButton

--- lib/note_list_route.dart
+++ lib/note_list_route.dart
@@ -4,6 +4,46 @@ import 'package:myapp/note_model.dart';
 import 'package:provider/provider.dart';
 
 import 'note_cell.dart';
+import 'note_detail_route.dart';
+
+class NoteList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<NoteModel>(builder: (context, noteModel, child) {
+      final cells = noteModel.notes.map((e) => NoteCell(e)).toList();
+      return ListView(children: cells);
+    });
+  }
+}
+
+class AddButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<NoteModel>(
+        builder: (context, noteModel, child) => CupertinoButton(
+              onPressed: () {
+                final note = noteModel.create();
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => NoteDetailRoute(note)),
+                );
+              },
+              child: Text("Add"),
+            ));
+  }
+}
+
+class BottomToolbar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [AddButton()],
+    );
+  }
+}
 
 class NoteListRoute extends StatelessWidget {
   @override
@@ -12,11 +52,7 @@ class NoteListRoute extends StatelessWidget {
       appBar: CupertinoNavigationBar(
         middle: Text("メモ"),
       ),
-      body: Consumer<NoteModel>(builder: (context, noteModel, child) {
-        final cells = noteModel.notes.map((e) => NoteCell(e)).toList();
-        return Column(
-            mainAxisAlignment: MainAxisAlignment.start, children: cells);
-      }),
+      body: Column(children: [Expanded(child: NoteList()), BottomToolbar()]),
     );
   }
 }
--- lib/note_model.dart
+++ lib/note_model.dart
@@ -1,4 +1,5 @@
 import 'dart:collection';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 
@@ -18,6 +19,13 @@ class NoteModel extends ChangeNotifier {
   void add(Note note) {
     _notes.add(note);
     notifyListeners();
+  }
+
+  Note create() {
+    final maxId = notes.map((e) => e.id).reduce(max);
+    final note = Note(maxId + 1, "");
+    add(note);
+    return note;
   }
 
   void remove(int id) {
